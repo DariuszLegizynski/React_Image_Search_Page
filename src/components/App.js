d20703a210dc93e3541cc78e1df9d92d5fa5aed8
DariuszLegizynski <d.legizynski@gmail.com>
2020-05-24 22:30:46 +0200

    made the app with hooks. Used useState and useEffect for this

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import SearchBar from './SearchBar';
 
-function App(){
 
-function onSearchSubmit(inputText){
-  console.log(inputText);
-}
+const App = () => {
+  const [images, setImages] = useState([]);
+
+  const onSearchSubmit = async (inputText) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: inputText},
+      headers: {
+        Authorization: "Client-ID LVdIXhfVQyl4XwYcKBZYwTsdLtDYVr4CyJ8LnqEXI5Y"
+      }
+    });
+    console.log(response);
+    setImages(response.data.results);
+  }
+
+  useEffect(() => {
+    	onSearchSubmit();
+  }, [])
 
   return (
     <div className="ui container" style={{marinTop: "10px"}}>
       
       <SearchBar whenSubmitted={onSearchSubmit} />
       {/* whenSubmitted is used because I can freely use a name, no restrictions */}
+      <p>{images.length}</p>
     </div>
   );
 }
